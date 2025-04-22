cbefc2cc5b6921105871e5b4939fa1db11c0629a
ricitos2001 <cesar2001ricitos@gmail.com>
2025-04-22 20:25:05 +0200

    plantilla examen

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// EJERCICIO 1
+// Cambiar el color de fondo al hacer clic en el botón "btnColor".
+
+
+// EJERCICIO 2
+// Agregar elementos a la lista de tareas usando inputTarea y btnAgregar.
+
+
+// EJERCICIO 3
+// Al hacer clic en "btnAñadirImagen", tomar la URL del input y crear una <img> en la galería.
+// Las imágenes deben poder eliminarse haciendo clic en ellas.
+
+
+// EJERCICIO 4
+// Validar el formulario: que nombre no esté vacío y el email sea válido.
+// Mostrar mensaje de éxito o error en "mensajeForm".
+
+
+// EJERCICIO 5
+// Al cambiar el filtro, mostrar solo los productos con la categoría seleccionada.
+// Recuerda que cada filtro debe funcionar correctamente.
+
+
+// EJERCICIO 6
+// Al hacer clic en los botones del catálogo, agregar el nombre del producto a la lista del carrito.
+// Se debe realizar con el uso de localStorage.
